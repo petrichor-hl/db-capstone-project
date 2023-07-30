eee96d9974e911e3e5b609c591a3a3ad5ee6df1e
petrichor-hl <tl.hoanglam174@gmail.com>
2023-07-30 22:39:02 +0700

    complete week 2

--- populate_data.sql
+++ populate_data.sql
@@ -1,6 +1,7 @@
 use littlelemondb;
 
 insert into customers (CustomerID, FullName, Email, ContactNumber) values
+(0, 'unknown', 'unknown', 'unknown'),
 (1, "Vanessa McCarthy", "vaMcCar@gmail.com", 013456789),
 (2, "Marcos Romero", "marRo@gmail.com", 013456789),
 (3, "Anna Iversen", "anIve@gmail.com", 013456789),
@@ -11,22 +12,23 @@ insert into customers (CustomerID, FullName, Email, ContactNumber) values
 
 INSERT INTO Staffs (StaffID, StaffName, Role, Address, Email, ContactNumber, AnnualSalary)
 VALUES
-(01, 'Mario Gollini', 'Manager', '724, Parsley Lane, Old Town, Chicago, IL', 'Mario.g@littlelemon.com', 351258074, '70000'),
-(02, 'Adrian Gollini', 'Assistant Manager', '334, Dill Square, Lincoln Park, Chicago, IL', 'Adrian.g@littlelemon.com', 351474048, '65000'),
-(03, 'Giorgos Dioudis', 'Head Chef', '879 Sage Street, West Loop, Chicago, IL', 'Giorgos.d@littlelemon.com', 351970582, '50000'),
-(04, 'Fatma Kaya', 'Assistant Chef', '132  Bay Lane, Chicago, IL', 'Fatma.k@littlelemon.com', 351963569, '45000'),
-(05, 'Elena Salvai', 'Head Waiter', '989 Thyme Square, EdgeWater, Chicago, IL', 'Elena.s@littlelemon.com', 351074198, '40000'),
-(06, 'John Millar', 'Receptionist', '245 Dill Square, Lincoln Park, Chicago, IL', 'John.m@littlelemon.com', 351584508, '35000');
+(0, 'unknown', 'unknown', 'unknown', 'unknown', 'unknown', 0),
+(1, 'Mario Gollini', 'Manager', '724, Parsley Lane, Old Town, Chicago, IL', 'Mario.g@littlelemon.com', 351258074, '70000'),
+(2, 'Adrian Gollini', 'Assistant Manager', '334, Dill Square, Lincoln Park, Chicago, IL', 'Adrian.g@littlelemon.com', 351474048, '65000'),
+(3, 'Giorgos Dioudis', 'Head Chef', '879 Sage Street, West Loop, Chicago, IL', 'Giorgos.d@littlelemon.com', 351970582, '50000'),
+(4, 'Fatma Kaya', 'Assistant Chef', '132  Bay Lane, Chicago, IL', 'Fatma.k@littlelemon.com', 351963569, '45000'),
+(5, 'Elena Salvai', 'Head Waiter', '989 Thyme Square, EdgeWater, Chicago, IL', 'Elena.s@littlelemon.com', 351074198, '40000'),
+(6, 'John Millar', 'Receptionist', '245 Dill Square, Lincoln Park, Chicago, IL', 'John.m@littlelemon.com', 351584508, '35000');
 -- select * from Staffs;
 
-INSERT INTO Bookings (BookingID, BookingDate, TableNo, BookingSlot, CustomerID, StaffID)
+INSERT INTO Bookings (BookingDate, TableNo, BookingSlot, CustomerID, StaffID)
 VALUES
-(1, '2022-04-17', 12, '19:00:00', 1, 1),
-(2, '2022-07-29', 12, '21:00:00', 2, 1),
-(3, '2023-01-07', 19, '15:00:00', 3, 3),
-(4, '2023-05-21', 15, '17:30:00', 4, 4),
-(5, '2023-05-27', 5, '18:30:00', 5, 2),
-(6, '2023-07-30', 8, '20:00:00', 6, 5);
+('2022-10-10', 5, '19:00:00', 1, 1),
+('2022-11-12', 3, '21:00:00', 2, 1),
+('2022-10-11', 2, '15:00:00', 3, 3),
+('2022-10-13', 2, '17:30:00', 4, 4),
+('2023-04-17', 1, '18:30:00', 5, 2),
+('2023-07-30', 8, '20:00:00', 6, 5);
 -- select * from Bookings;
 
 INSERT INTO MenuItems (MenuItemID, CourseName, StarterName, DessertName, Drink, Price)
